0b402117a208ebebe0b8b2f202a04bffa2b66205
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2023-06-14 18:36:45 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,9 +93,9 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
- 
-            text = START_MSG.format(
+        await message.reply_photo(
+            photo = IMG_START
+             START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
@@ -107,11 +107,7 @@ async def start_command(client: Client, message: Message):
             quote = True
             
       
-        update.effective_message.reply_photo(
-            IMG_START, caption= "<code>OwO Onichan! I am ready to play, hehe~</code>: <code>{}</code>".format(
-                uptime
-            ),
-            parse_mode=ParseMode.HTML,
+       
         )
         return
 
