dddb1cc08cdaf234c16917cc1c221e6661b4b8ac
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2023-06-14 18:50:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,7 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
-IMG_START = START_IMG.split(".") 
+
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
@@ -94,7 +94,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_photo(
-            photo = IMG_START
+           
             text =  START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
