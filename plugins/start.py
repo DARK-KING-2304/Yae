5a6ebd7c260998fbc5a1919abe137948b25ee011
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2023-06-15 13:17:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("JOIN CHANNEL", url = "https://t.me/Anime_Crimson")
+                    InlineKeyboardButton("🌸JOIN CHANNEL🌸", url = "https://t.me/Anime_Crimson")
                     
                 ]
             ]
