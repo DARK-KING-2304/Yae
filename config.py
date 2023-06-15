f16282e8156cb0f1d0268b9a1c77ec795857b4cb
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2023-06-15 13:05:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "13849191"))
 API_HASH = os.environ.get("API_HASH", "c1b434defccacad6063758c9a7d76d5d")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001853425881"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001863828346"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5109376623"))
@@ -39,7 +39,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_IMG = os.environ.get("START_IMAGE", "https://graph.org//file/e54e0fad1b478e43c84a4.jpg") 
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "• 𝗞𝗼𝗻𝗻𝗶𝗰𝗵𝗶𝘄𝗮 {first} 💫\n\n• 𝗬𝗼𝘂 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗼𝘂𝗿 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 [ 𝗖𝗹𝗶𝗰𝗸 𝗼𝗻 𝗷𝗼𝗶𝗻 ] 𝘁𝗵𝗲𝗻\n\n • 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗯𝘆 𝘁𝗮𝗽𝗽𝗶𝗻𝗴 𝗼𝗻 📥 𝗥𝗲𝗹𝗼𝗮𝗱 📥 𝗞𝗲𝗲𝗽 𝗦𝘂𝗽𝗽𝗼𝗿𝘁𝗶𝗻𝗴 😊\n\n</b>")
+START_MSG = os.environ.get("START_MESSAGE", "• 𝗞𝗼𝗻𝗻𝗶𝗰𝗵𝗶𝘄𝗮 {first} 💫\n\n• 𝗬𝗼𝘂 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗼𝘂𝗿 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 [ 𝗖𝗹𝗶𝗰𝗸 𝗼𝗻 𝗷𝗼𝗶𝗻 ] 𝘁𝗵𝗲𝗻\n\n• 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗯𝘆 𝘁𝗮𝗽𝗽𝗶𝗻𝗴 𝗼𝗻 📥 𝗥𝗲𝗹𝗼𝗮𝗱 📥 𝗞𝗲𝗲𝗽 𝗦𝘂𝗽𝗽𝗼𝗿𝘁𝗶𝗻𝗴 😊\n\n</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "5122071509 5850249548 5852955057 1730665149").split()):
@@ -48,7 +48,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "• 𝗞𝗼𝗻𝗻𝗶𝗰𝗵𝗶𝘄𝗮 {first} 💫\n\n• 𝗬𝗼𝘂 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗼𝘂𝗿 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 [ 𝗖𝗹𝗶𝗰𝗸 𝗼𝗻 𝗷𝗼𝗶𝗻 ] 𝘁𝗵𝗲𝗻\n\n • 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗯𝘆 𝘁𝗮𝗽𝗽𝗶𝗻𝗴 𝗼𝗻 📥 𝗥𝗲𝗹𝗼𝗮𝗱 📥 𝗞𝗲𝗲𝗽 𝗦𝘂𝗽𝗽𝗼𝗿𝘁𝗶𝗻𝗴 😊\n\n</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "• 𝗞𝗼𝗻𝗻𝗶𝗰𝗵𝗶𝘄𝗮 {first} 💫\n\n• 𝗬𝗼𝘂 𝗻𝗲𝗲𝗱 𝘁𝗼 𝗷𝗼𝗶𝗻 𝗼𝘂𝗿 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 [ 𝗖𝗹𝗶𝗰𝗸 𝗼𝗻 𝗷𝗼𝗶𝗻 ] 𝘁𝗵𝗲𝗻\n\n• 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗯𝘆 𝘁𝗮𝗽𝗽𝗶𝗻𝗴 𝗼𝗻 📥 𝗥𝗲𝗹𝗼𝗮𝗱 📥 𝗞𝗲𝗲𝗽 𝗦𝘂𝗽𝗽𝗼𝗿𝘁𝗶𝗻𝗴 😊\n\n</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
